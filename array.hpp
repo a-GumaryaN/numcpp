1c7837a6eff2b8e69e0d0dda3a1f21cc6c88f8fd
gumaryan <alireza313gomarian@gmail.com>
2024-04-21 00:34:41 +0330

    add read_array functionality to array.hpp

--- array.hpp
+++ array.hpp
@@ -4,6 +4,18 @@
 
 using namespace std;
 
+bool isdigit(char ch)
+{
+    if (int(ch) >= 48 && int(ch) <= 57)
+        return true;
+    return false;
+}
+
+int to_digit(char ch)
+{
+    return int(ch) - 48;
+}
+
 class Array
 {
 public:
@@ -331,3 +343,59 @@ public:
         return sqrt(variance);
     }
 };
+
+Array *read_array(std::string str)
+{
+    Array *new_array = new Array;
+    bool isNumber = false;
+    bool isFloat = false;
+    int depth = 0;
+    double temp_number = 0;
+    int decimal_number = 0;
+    for (char c : str)
+    {
+        if (isdigit(c))
+        {
+            isNumber = true;
+            if (isFloat)
+            {
+                temp_number = temp_number * 10;
+                temp_number += to_digit(c);
+                decimal_number += 1;
+                continue;
+            }
+            else
+            {
+                temp_number = temp_number * 10;
+                temp_number += to_digit(c);
+                continue;
+            }
+        }
+        if (c == '.')
+        {
+            isFloat = true;
+            continue;
+        }
+        if (c == '[')
+        {
+            depth += 1;
+            continue;
+        }
+        if (c == ']')
+        {
+            depth -= 1;
+        }
+        if ((c == ',' || c == ']') && isNumber == true)
+        {
+            temp_number = temp_number / pow(10, decimal_number);
+            new_array->add(temp_number);
+            temp_number = 0;
+            decimal_number = 0;
+            isFloat = false;
+            isNumber = false;
+            continue;
+        }
+    }
+
+    return new_array;
+}
